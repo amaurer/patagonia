e220eef413a64449ca1a932732a8db410fbe7666
Bryan Leonard <b.leonard@zoho.com>
2015-03-14 18:34:51 -0400

    Update catalog page title spacing.

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,22 @@ router.get('/', function(req, res) {
 
 
 router.get('/beds/', function(req, res) {
-  res.render('catalog', { title: "Beds", data : global.catalogCollection.beds, section : "beds" });
+  res.render('catalog', { title: "Beds - ", data : global.catalogCollection.beds, section : "beds" });
 });
 router.get('/sofas/', function(req, res) {
-  res.render('catalog', { title: "Sofas", data : global.catalogCollection.sofas, section : "sofas" });
+  res.render('catalog', { title: "Sofas - ", data : global.catalogCollection.sofas, section : "sofas" });
 });
 router.get('/occasional-chairs/', function(req, res) {
-  res.render('catalog', { title: "Occasional Chairs", data : global.catalogCollection["occasional-chairs"], section : "occasional-chairs" });
+  res.render('catalog', { title: "Occasional Chairs - ", data : global.catalogCollection["occasional-chairs"], section : "occasional-chairs" });
 });
 router.get('/dining-chairs-bonquetes/', function(req, res) {
-  res.render('catalog', { title: "Dining Chairs and Banquetes", data : global.catalogCollection["dining-chairs-tables"], section : "dining-chairs-tables" });
+  res.render('catalog', { title: "Dining Chairs and Banquetes - ", data : global.catalogCollection["dining-chairs-tables"], section : "dining-chairs-tables" });
 });
 router.get('/chase-ottoman/', function(req, res) {
-  res.render('catalog', { title: "Chases and Ottomans", data : global.catalogCollection["chases-ottoman"], section : "chases-ottoman" });
+  res.render('catalog', { title: "Chases and Ottomans - ", data : global.catalogCollection["chases-ottoman"], section : "chases-ottoman" });
 });
 router.get('/chase-goods/', function(req, res) {
-  res.render('catalog', { title: "Chase Goods", data : global.catalogCollection["chase-goods"], section : "chase-goods" });
+  res.render('catalog', { title: "Chase Goods - ", data : global.catalogCollection["chase-goods"], section : "chase-goods" });
 });
 router.get('/product/:catalog/:product', function(req, res) {
   res.render('product', { title: "", data : global.catalogCollection[req.params.catalog][req.params.product] });
