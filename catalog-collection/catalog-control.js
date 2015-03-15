71ff791336f293348f75639fdb8026585775bb2e
Andrew Maurer <andrew@maurer.me>
2015-03-15 17:10:09 -0400

    Changed to use the images at a catalog

--- catalog-collection/catalog-control.js
+++ catalog-collection/catalog-control.js
@@ -10,7 +10,24 @@ module.exports = function buildCatalog(imagePath){
 	var n, nn, i, data, fileSplit, root, files, temp;
 	//read this directory, build catalog, loop image directory and assign
 
+	// NEW
+	root = "public/catalog";
+	files = fs.readdirSync(root);
+	for (i = files.length - 1; i >= 0; i--) {
+		if(files[i].indexOf("-lg.") === -1) continue;
+		fileSplit = files[i].split(".");
+		if(fileSplit[1] !== "jpg") continue;
+		temp = fileSplit[0].toLowerCase().split("-").splice(0,2);
+		if(catalog[temp[0]] == null) catalog[temp[0]] = [];
+		catalog[temp[0]].push(temp[1]);
+	};
+
+	return catalog;
+
+
+
 	// INDEX CATALOG
+	// OLD CATALOG
 	root = "catalog-collection";
 	files = fs.readdirSync(root);
 	for (i = files.length - 1; i >= 0; i--) {
@@ -37,8 +54,11 @@ module.exports = function buildCatalog(imagePath){
 		// matching logic
 		temp = fileSplit[0].toLowerCase().split("-").splice(0,2);
 		for(n in catalog){
+			var counter = 0;
 			for(nn in catalog[n]){
 				if(nn.toLowerCase().indexOf(temp[1]) !== -1){
+					counter++
+					if(counter >1) console.log(nn, temp[1] + "-" + temp[0], counter)
 					catalog[n][nn].images.push(files[i]);
 				} else {
 					//console.log("NOT FOUND!!!! " + files[i])
@@ -48,9 +68,9 @@ module.exports = function buildCatalog(imagePath){
 
 	};
 
-	return catalog
-	// Missing Images
+	return catalog;
 
+	// Missing Images
 	for(n in catalog){
 		for(nn in catalog[n]){
 			if(catalog[n][nn].images.length === 0){
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,62 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: "Welcome - " });
+router.get("/", function(req, res) {
+  res.render("index", { title: "Welcome - " });
 });
 
+router.get("/products/:section/", function(req, res) {
+  // Remove plural
+  var temp = req.params.section;
+  if(req.params.section.substr(req.params.section.length-1) === "s"){
+  	temp = req.params.section.substr(0, req.params.section.length -1);
+  };
+  res.render("catalog", {
+  	//title: req.params.section.substr(0,1).toUpperCase() + req.params.section.substr(1),
+  	title: req.params.section,
+  	data : global.catalogCollection[temp],
+  	section : temp
+  });
+});
 
-router.get('/beds/', function(req, res) {
-  res.render('catalog', { title: "Beds", data : global.catalogCollection.beds, section : "beds" });
+router.get("/products/:section/:product/", function(req, res) {
+  // Remove plural
+  var temp = req.params.section;
+  if(req.params.section.substr(req.params.section.length-1) === "s"){
+  	temp = req.params.section.substr(0, req.params.section.length -1);
+  };
+  res.render("product", {
+  	title: req.params.product,
+  	data : global.catalogCollection[temp][req.params.product],
+  	section : temp,
+  	product : req.params.product
+  });
 });
-router.get('/sofas/', function(req, res) {
-  res.render('catalog', { title: "Sofas", data : global.catalogCollection.sofas, section : "sofas" });
+
+/*
+router.get("/beds/", function(req, res) {
+  res.render("catalog", { title: "Beds", data : global.catalogCollection.beds, section : "beds" });
 });
-router.get('/occasional-chairs/', function(req, res) {
-  res.render('catalog', { title: "Occasional Chairs", data : global.catalogCollection["occasional-chairs"], section : "occasional-chairs" });
+router.get("/sofas/", function(req, res) {
+  res.render("catalog", { title: "Sofas", data : global.catalogCollection.sofas, section : "sofas" });
 });
-router.get('/dining-chairs-bonquetes/', function(req, res) {
-  res.render('catalog', { title: "Dining Chairs and Banquetes", data : global.catalogCollection["dining-chairs-tables"], section : "dining-chairs-tables" });
+router.get("/occasional-chairs/", function(req, res) {
+  res.render("catalog", { title: "Occasional Chairs", data : global.catalogCollection["occasional-chairs"], section : "occasional-chairs" });
 });
-router.get('/chase-ottoman/', function(req, res) {
-  res.render('catalog', { title: "Chases and Ottomans", data : global.catalogCollection["chases-ottoman"], section : "chases-ottoman" });
+router.get("/dining-chairs-bonquetes/", function(req, res) {
+  res.render("catalog", { title: "Dining Chairs and Banquetes", data : global.catalogCollection["dining-chairs-tables"], section : "dining-chairs-tables" });
 });
-router.get('/chase-goods/', function(req, res) {
-  res.render('catalog', { title: "Chase Goods", data : global.catalogCollection["chase-goods"], section : "chase-goods" });
+router.get("/chase-ottoman/", function(req, res) {
+  res.render("catalog", { title: "Chases and Ottomans", data : global.catalogCollection["chases-ottoman"], section : "chases-ottoman" });
 });
-router.get('/product/:catalog/:product', function(req, res) {
-  res.render('product', { title: "", data : global.catalogCollection[req.params.catalog][req.params.product] });
+router.get("/chase-goods/", function(req, res) {
+  res.render("catalog", { title: "Chase Goods", data : global.catalogCollection["chase-goods"], section : "chase-goods" });
 });
+
+router.get("/products/:section/:product", function(req, res) {
+  res.render("product", { title: "", data : global.catalogCollection[req.params.section][req.params.product] });
+});
+*/
 
 
 module.exports = router;
